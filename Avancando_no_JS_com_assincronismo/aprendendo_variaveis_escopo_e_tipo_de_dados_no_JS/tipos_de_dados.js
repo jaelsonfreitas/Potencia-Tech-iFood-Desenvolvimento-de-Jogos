1f90158cbd85949b59b0adee3226521c981e80e9
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-12-20 12:30:20 -0300

    Aula aprendendo variaveis, escopo e tipos de dados no JS.

---
+++ Avancando_no_JS_com_assincronismo/aprendendo_variaveis_escopo_e_tipo_de_dados_no_JS/tipos_de_dados.js
@@ -0,0 +1,20 @@
+// +infinity - Ininity e NAn
+// Double Precision 64 - bit binary forat IEE 754
+let sum = 0.1
+let sum2 = 0.3
+
+let soma = sum + sum2
+
+console.log(soma);
+
+// Para declarar strings temos 03 formas
+// aspas duplas "..."
+// aspas simples  '...'
+// template laterals `...`
+
+// convensão aspas simples e para concatenar coisas com template literals
+
+const id = 20;
+
+console.log('<div id = "' + id +'"> teste <div>');
+console.log(`<div id = "${id}"> test <div>`);
