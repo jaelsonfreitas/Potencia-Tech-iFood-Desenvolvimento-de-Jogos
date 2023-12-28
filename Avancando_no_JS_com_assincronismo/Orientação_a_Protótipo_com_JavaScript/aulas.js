7254246bbc6ec2ed2a1c9dad05237d785d370634
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-12-28 15:04:27 -0300

    Renomeando arquivo e conteúdo da primeira aula.

---
+++ Avancando_no_JS_com_assincronismo/Orientação_a_Protótipo_com_JavaScript/aulas.js
@@ -0,0 +1,25 @@
+
+// const pessoa = {
+//     genero: 'masculino'
+// }
+// const jaelson = Object.create(pessoa);
+//     nome: 'Jaelson';
+//     idade: 30;
+
+
+// console.log(jaelson.genero);
+
+// function Pessoa(nome, idade) {
+//     this.nome = nome
+//     this.idade = idade
+// }
+// Pessoa.prototype.falar = function () {
+//     console.log(`Meu nome é: ${this.nome}`);
+// }
+// const thainah = new Pessoa('Thainah', 25)
+
+
+// thainah.falar()
+
+// Aula 02 Entendendo Sobrescrita e Shadowing
+
